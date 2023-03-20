2cdf478432cdeb06a799d59510ee475bd4a84772
subham-byte <subhama3650@gmail.com>
2023-03-20 15:15:26 +0530

    Accordion using React.

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Accordion from "./Accordion";
+import "./App.css";
+import { accordionData } from "./utils/Content";
+
+const App = () => {
+
+  //dumy data to be used.
+  // const accordionData = [
+  //   {
+  //     title: 'Section 1',
+  //     content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+  //     laborum cupiditate possimus labore, hic temporibus velit dicta earum
+  //     suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+  //     voluptatem.`
+  //   },
+  //   {
+  //     title: 'Section 2',
+  //     content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+  //     reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+  //     quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+  //     pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+  //     quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+  //     Repudiandae, mollitia id reprehenderit a ab odit!`
+  //   },
+  //   {
+  //     title: 'Section 3',
+  //     content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
+  //     quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
+  //     dolor ut sequi minus iste? Quas?`
+  //   }
+  // ];
+
+  return (
+    <>
+      <h1>React Accordion Demo</h1>
+      <div className="accordion">
+        {accordionData.map(({title, content}) => (
+          <Accordion title = {title} content = {content}/>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default App;
